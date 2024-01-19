2a027acf6a29b54b555efe7ea6d455ee84036f92
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-19 22:10:06 +0530

    Understand fundamentals of Programming Language

---
+++ Let's Learn C++/00-Introduction.cpp
@@ -0,0 +1,39 @@
+/*
+    Programming Language : A Programming Language is a Computer Language that
+                is used by programmers to interact with the Computers. It's
+                set of Instruction written by any language eg. C++, Java or Python
+                to Perform a Specific Task.
+
+    Compiler : We Know that computer understand only Binary Language (Zero or One)
+                and if we want to interact with the computer then we need to give
+                instruction in binary language, but we can't understand binary language
+                we understand only normal langauge Like English or Hindi which is high
+                level language, So how can we give instruction to the computer for this
+                compiler is introduced which converts our high level english language in
+                to the low level language so that we can communicate with Computers in
+                easy manner and process of converting this higher level language into
+                lower level language is called compilation process.
+*/
+/*
+    What is C++ : C++ is high level Programming Language created by Danish Computer
+                Scientist Bjarne stroustrup.
+
+    Why C++ :
+        - Supports Objected Oriented Programming
+        - Control Over System Resources and Memory
+        - High Performance and Efficient
+
+    Applications of C++ :
+        - System Software Development
+        - Game Development
+        - Real time System
+*/
+
+/*
+    Source Code : Any Instruction written in Programming Language is Called
+            Source Code
+
+    Executable Code : Binary Code is Called Executable Code which is Generated
+            After the Compilation Process because computer understand only binary
+            language
+*/
