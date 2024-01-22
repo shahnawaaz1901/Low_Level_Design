3f56061c85ceb09003fb03d2385bae05d565c059
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-23 01:13:28 +0530

    Start to Understand Variable in C++

---
+++ Let's Learn C++/02-Variables_and_Data_Types.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*
+        In Programming We Many Data is Interchange and access, But how can we
+        identify that we need to a certain data because data is much much, so
+        how can we know that this is the data what we want, So for this problem
+        a Concept name "Variable" is introduced, Which is nothing but a name of
+        the data, that name is linked with the memory where data is stored so
+        that we can able to access the data using Variable, But in case of variable
+        we can change the value of variable whenever we want to change
+    */
+
+    /*
+        In C++ when We Want to Declare a Variable we need to first Specify the data
+        type of the data then we write the name of the variable whatever we want then
+        we assign it to some value. But now question is what is Data Type, Data type
+        is an identifier which denotes which type of data user want to store, our
+        compiler is also assign memory to a varible by checking the data type of the
+        Variable, Because different different kind of data needs different different
+        size in the memory
+    */
+
+    int x = 10;
+    /*
+        Understand this line One by One
+        first we write "int", Because C++ is statically typed Language so that we need
+        to specify the data type by the value which we want to store, so first we write
+        "int" which is data type next we write "x", this x is nothing but a variable name
+        means when ever we want to access this data we can access by using the name of
+        variable "x", equal sign use to assign some value in the variable, means the actual
+        value which we want to store.
+    */
+}
